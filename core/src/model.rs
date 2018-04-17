a02aee170a7c1909232e47faa35d691713920f34
zuowenjian <zuowenjian@outlook.com>
2018-04-17 13:26:17 +0800

    work for #4

--- core/src/model.rs
+++ core/src/model.rs
@@ -13,7 +13,6 @@ impl Context
     }
     pub fn rollback(&self)
     {
-
     }
 
 
@@ -23,30 +22,36 @@ pub trait CliCmd
 {
 
 }
-pub trait Icontrol {
-    fn _allow(&self  ) ->BoolR;
-    fn _before(&self ) ->BoolR;
-    fn _after(&self  ) ->BoolR;
-    fn _start(&self  ) ->BoolR;
-    fn _stop(&self   ) ->BoolR;
-    fn _check(&self  ) ->BoolR;
-    fn _clean(&self  ) ->BoolR;
-    fn _info(&self   ) ->BoolR;
-}
-pub trait DefaultCtrl
+pub trait CallPlugin 
 {
-
+    fn res_before(&self ) ->BoolR;
+    fn res_after(&self  ) ->BoolR;
+}
+pub trait StopBehavior
+{
+    fn res_stop(&self   ) ->BoolR;
+    fn res_check(&self  ) ->BoolR;
+    fn res_clean(&self  ) ->BoolR;
+}
+pub trait StartBehavior
+{
+    fn res_allow(&self  ) ->BoolR;
+    fn res_conf(&self   ) ->BoolR;
+    fn res_start(&self  ) ->BoolR;
+    fn res_info(&self   ) ->BoolR;
 }
 
 pub trait Res
 {
     fn allow(&self  ) ->BoolR;
+    fn conf(&self) ->BoolR ;
     fn start(&self  ) ->BoolR;
     fn stop(&self   ) ->BoolR;
     fn check(&self  ) ->BoolR;
     fn clean(&self  ) ->BoolR;
     fn info(&self   ) ->BoolR;
 }
+pub type ResBox = Box<Res> ;
 
 type ResMap = HashMap<String,Box<Res>> ;
 pub struct ResCollection
@@ -55,166 +60,142 @@ pub struct ResCollection
 }
 
 impl<T> Res for T  
-    where T: Icontrol
+where T: StartBehavior + StopBehavior + CallPlugin
 {
     fn allow(&self) ->BoolR 
     {
-        self._allow() 
+        self.res_allow() 
     }
+    fn conf(&self) ->BoolR 
+    {
+        self.res_before()? ;
+        self.res_conf()? ;
+        self.res_after()?;
+        Ok(())
+    }
+
     fn start(&self) ->BoolR 
     {
-        self._before()? ;
-        self._start()? ;
-        self._after()?;
+        self.res_before()? ;
+        self.res_start()? ;
+        self.res_after()?;
         Ok(())
     }
 
     fn stop(&self) ->BoolR 
     {
-        self._before()? ;
-        self._stop()? ;
-        self._after()? ;
+        self.res_before()? ;
+        self.res_stop()? ;
+        self.res_after()? ;
         Ok(())
     }
     fn check(&self) ->BoolR 
     {
-        self._before()? ;
-        self._check()? ;
-        self._after()? ;
+        self.res_before()? ;
+        self.res_check()? ;
+        self.res_after()? ;
         Ok(())
 
     }
     fn clean(&self) ->BoolR 
     {
-        self._before()? ;
-        self._clean()? ;
-        self._after()? ;
+        self.res_before()? ;
+        self.res_clean()? ;
+        self.res_after()? ;
         Ok(())
 
     }
     fn info(&self) ->BoolR 
     {
-        self._before()? ;
-        self._info()? ;
-        self._after()? ;
+        self.res_before()? ;
+        self.res_info()? ;
+        self.res_after()? ;
         Ok(())
-    }
-
-}
-impl <T> Icontrol for  T 
- where T : DefaultCtrl
-{
-    fn _allow(&self)  -> BoolR
-    {
-        trace!("DefaultCtrl::_allow") ;
-        Ok(())
-    }
-    fn _before(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_before") ;
-        Ok(())
-    }
-    fn _after(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_after") ;
-        Ok(())
-
-    }
-    fn _start(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_start") ;
-        Ok(())
-
-    }
-    fn _stop(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_stop") ;
-        Ok(())
-
-    }
-    fn _check(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_check") ;
-        Ok(())
-
-    }
-    fn _clean(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_clean") ;
-        Ok(())
-
-    }
-    fn _info(&self) -> BoolR
-    {
-        trace!("DefaultCtrl::_info") ;
-        Ok(())
-
     }
 }
+
 
 #[cfg(test)]
 mod tests
 {
     use super::* ;
+    use pretty_env_logger ;
     struct StubRes {
-
+    }
+    impl StubRes {
+        fn new() -> StubRes { StubRes{} }
     }
     //impl DefaultCtrl for StubRes {} 
-    impl Icontrol for StubRes
+    impl StartBehavior for StubRes
     {
-
-        fn _allow(&self) ->BoolR 
+        fn res_allow(&self) ->BoolR 
         {
-
-         Ok(())
-
-        }
-        fn _before(&self) ->BoolR 
-        {
-
-         Ok(())
-
-        }
-        fn _after(&self) ->BoolR 
-        {
-            trace!("StubRes::_after") ;
             Ok(())
 
         }
-        fn _start(&self) ->BoolR 
+        fn res_start(&self) ->BoolR 
         {
-         Ok(())
+            Ok(())
 
         }
-        fn _stop(&self) ->BoolR 
+        fn res_conf(&self) ->BoolR 
         {
-         Ok(())
+            Ok(())
 
         }
-        fn _check(&self) ->BoolR 
+        fn res_info(&self) ->BoolR 
         {
-         Ok(())
-
-        }
-        fn _clean(&self) ->BoolR 
-        {
-         Ok(())
-
-        }
-        fn _info(&self) ->BoolR 
-        {
-         Ok(())
+            Ok(())
 
         }
     }
+    impl StopBehavior for StubRes 
+    {
+        fn res_stop(&self) ->BoolR 
+        {
+            Ok(())
 
-    use pretty_env_logger ;
+        }
+        fn res_clean(&self) ->BoolR 
+        {
+            Ok(())
+
+        }
+        fn res_check(&self) ->BoolR 
+        {
+            Ok(())
+
+        }
+
+    }
+    impl CallPlugin for StubRes 
+    {
+        fn res_before(&self) ->BoolR 
+        {
+            Ok(())
+        }
+
+        fn res_after(&self) ->BoolR 
+        {
+            trace!("StubRes::res_after") ;
+            Ok(())
+
+        }
+    }
+    fn resres_check<T>( res :&T) where T : Res
+    {
+        assert!(res.info().is_ok()) ;
+        assert!(res.conf().is_ok()) ;
+        assert!(res.start().is_ok());
+        assert!(res.stop().is_ok()) ;
+        assert!(res.clean().is_ok()) ;
+    }
     #[test]
-    fn use_stub()
+    fn useres_stub()
     {
 
         pretty_env_logger::init();
-        let res = StubRes{} ;
-        res.start() ;
+        let res = StubRes::new();
+        resres_check(&res) ;
     }
 }
